8ff122e187f4ed73ca44b66e9401055035c54e17
adam-gatherer <adam@gatherer.tech>
2023-07-14 11:44:24 +0000

    Added html page, added templates folder & index.html

--- tempercent_app/app.py
+++ tempercent_app/app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 from .main import main_function
-    
 app = Flask(__name__)
+
+
+@app.route('/success/<post_code>')
+def success(post_code):
+    return f'<h1>{main_function(post_code)}</h1>'
+
+
+@app.route('/postcode',methods = ['POST', 'GET'])
+def index_page():
+    if request.method == 'POST':
+        postcode = request.form['postcode']
+        return redirect(url_for('success', post_code = postcode))
+    else:
+        postcode = request.args.get('postcode')
+        return redirect(url_for('success', post_code = postcode))
+
 
 @app.route("/")
 def main_page():
-    out_str = main_function("EH51SG")
-    return f"<h1>{out_str}</h1>"
+    #out_str = main_function("EH51SG")
+    #return f"<h1>{out_str}</h1>"
+    return render_template('index.html')
     
     
 @app.route("/api")
@@ -14,6 +30,5 @@ def api_page():
     return "Under construction lol"
 
 
-#@app.route("/api", postcode=str)
-#def api_post(postcode):
-#    return postcode
+if __name__ == '__main__':
+    app.run(debug = True)
